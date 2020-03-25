059a390657be129b41a588cc08ef26ed254ea393
Precious King <precieuxking9@gmail.com>
2020-03-25 20:03:54 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var name ="Precious King";
+console.log("My name is " + name)
+var courses =["HTML", "CSS", "JAVASCRIPT"];
+console.log( "I offer the following courses; " + courses);
+
+for(
+    let i = 0; i <200; i++
+)
+if(courses.length % 2 == 0 && i % 2 == 0){
+    console.log(i +=2);
+}
+else {
+    console.log(i +=1)
+}
+    
+
